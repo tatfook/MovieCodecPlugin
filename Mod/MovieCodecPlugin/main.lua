242fce036472c345cf63c934b04d26ce946d5683
LiXizhi <lixizhi@yeah.net>
2017-07-19 22:08:01 +0800

    manual install version

---
+++ Mod/MovieCodecPlugin/main.lua
@@ -0,0 +1,45 @@
+--[[
+Title: 
+Author(s):  
+Date: 
+Desc: 
+use the lib:
+------------------------------------------------------------
+NPL.load("(gl)Mod/MovieCodecPlugin/main.lua");
+local MovieCodecPlugin = commonlib.gettable("Mod.MovieCodecPlugin");
+------------------------------------------------------------
+]]
+local MovieCodecPlugin = commonlib.inherit(commonlib.gettable("Mod.ModBase"),commonlib.gettable("Mod.MovieCodecPlugin"));
+
+function MovieCodecPlugin:ctor()
+end
+
+-- virtual function get mod name
+
+function MovieCodecPlugin:GetName()
+	return "MovieCodecPlugin"
+end
+
+-- virtual function get mod description 
+
+function MovieCodecPlugin:GetDesc()
+	return "MovieCodecPlugin is a plugin in paracraft"
+end
+
+function MovieCodecPlugin:init()
+	LOG.std(nil, "info", "MovieCodecPlugin", "plugin initialized");
+end
+
+function MovieCodecPlugin:OnLogin()
+end
+-- called when a new world is loaded. 
+
+function MovieCodecPlugin:OnWorldLoad()
+end
+-- called when a world is unloaded. 
+
+function MovieCodecPlugin:OnLeaveWorld()
+end
+
+function MovieCodecPlugin:OnDestroy()
+end
--- readme.md
+++ readme.md
@@ -3,3 +3,17 @@
 Author: LiXizhi
 Date: 2014.5
 
+### How To Build
+Add environment variable `NPLRUNTIME_ROOT` that point to git root of NPLRuntime.
+
+Open this folder with visual studio 2017 or above and build the cmake file.
+The output is in folder `Mod/MovieCodecPlugin/MovieCodecPlugin.dll` along with some prebuild dll files
+
+### How to Install
+Open paracraft's mod site and install MovieCodecPlugin
+
+### How to Install Manually?
+Open paracraft and copy `Mod/MovieCodecPlugin/*.dll` to `[paracraft]/Mod/MovieCodecPlugin/`
+
+### How to Run
+Press F9 or use `/recoder` command to launch the movie codec. 
